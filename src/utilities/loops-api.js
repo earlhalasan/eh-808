29405ab2877372305f04ec0fdd560b1d7a27e7f4
earlhalasan <earl.halasan@gmail.com>
2022-07-26 10:52:09 -0400

    set up loops api

---
+++ src/utilities/loops-api.js
@@ -0,0 +1,38 @@
+import { getToken } from "./users-service";
+
+const BASE_URL = "/api/loops";
+
+export function getAll() {
+  return sendRequest(BASE_URL);
+}
+
+export function deleteLoop(id) {
+  return sendRequest(`${BASE_URL}/deleteLoop/${id}`, "DELETE");
+}
+
+export function editLoop(id) {
+  return sendRequest(`${BASE_URL}/editLoop/${id}`, "PUT");
+}
+
+// Add an cat to the cat list
+export function newLoop(loopData) {
+  // Just send itemId for best security (no pricing)
+  // console.log(catData)
+  return sendRequest(`${BASE_URL}/newLoop`, "POST", loopData);
+}
+
+/*--- Helper Functions ---*/
+
+async function sendRequest(url, method = "GET", payload = null) {
+  const options = { method };
+  if (payload) {
+    options.headers = { "Content-Type": "application/json" };
+    options.body = JSON.stringify(payload);
+  }
+  const res = await fetch(url, options);
+  // res.ok will be false if the status code set to 4xx in the controller action
+  console.log("sedning request");
+  if (res.ok) return res.json();
+  console.log(res.json);
+  throw new Error("Bad Request");
+}
